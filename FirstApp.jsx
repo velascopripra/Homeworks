1caf946a30ab01afb05815ff313b351e2f5f0905
velascopripra <juan_d.velasco@uao.edu.co>
2025-03-12 13:41:54 -0500

    Add files via upload

---
+++ FirstApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const titulo = "First App";
+
+
+const FirstApp = () => {
+  return (
+    <div>
+      <h1>{titulo} </h1>
+      <span>10</span>
+    </div>
+  );
+};
+
+export default FirstApp;
